bc81cdfc940014b6ad56c009f374e90dc3b0cbee
Scott Lee <ji6.lee@samsung.com>
2019-11-01 11:00:08 +0900

    Init conky settings

---
+++ conky-startup.sh
@@ -0,0 +1,12 @@
+#sleep 20s
+killall conky
+cd "/home/meghendra/.conky/Green Apple Desktop"
+conky -c "/home/meghendra/.conky/Green Apple Desktop/Gotham" &
+cd "/home/meghendra/.conky/TeejeeTech"
+conky -c "/home/meghendra/.conky/TeejeeTech/CPU Panel (8-core)" &
+cd "/home/meghendra/.conky/TeejeeTech"
+conky -c "/home/meghendra/.conky/TeejeeTech/NVIDIA Panel" &
+cd "/home/meghendra/.conky/TeejeeTech"
+conky -c "/home/meghendra/.conky/TeejeeTech/Network Panel" &
+cd "/home/meghendra/.conky/TeejeeTech"
+conky -c "/home/meghendra/.conky/TeejeeTech/Process Panel" &
